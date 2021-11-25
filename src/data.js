10da48080ac13bf68d6dcbdc86ab6681c64a07c3
daryllborn <born.dd@gmail.com>
2021-11-25 14:15:36 +0100

    Added space invaders to projects

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const featuredPortfolio = [
       title: "Ecommerce frontend",
       img: "/assets/bornshop.png",
       desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      skills: ["React", "Styled components", "Axios", "Stripe"],
+      skills: ["React", "Styled components", "Axios", "Stripe", "HTML", "CSS", "JavaScript"],
       code: "https://github.com/daryllborn?tab=repositories",
       demo: "https://bornshop.netlify.app/"
     },
@@ -19,12 +19,12 @@ export const featuredPortfolio = [
     },
     {
       id: 3,
-      title: "Ecommerce frontend",
+      title: "Space Invaders",
       img: "/assets/bornshop.png",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      skills: ["React", "Styled components", "Axios", "Stripe"],
-      code: "https://github.com/daryllborn?tab=repositories",
-      demo: "https://bornshop.netlify.app/"
+      desc: "A simple game made with pure HTML, CSS, JavaScript and some JQuery to practice Object Oriented programming principles and just have some fun!",
+      skills: ["HTML", "CSS", "JavaScript", "JQuery"],
+      code: "https://github.com/daryllborn/spaceinvaders",
+      demo: "https://spaceinvaders0.netlify.app/"
     },
     {
       id: 4,
